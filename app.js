56a7d870be13356d7142eb92c022697e99140113
tsztin0217 <tsztin0217@gmail.com>
2025-02-17 20:29:01 -0500

    added user story and created constants

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+/*
+
+Here are the user stories for this lab:
+
+As a user, I want to be able to select numbers so that I can perform operations with them.
+As a user, I want to be able to add two numbers together.
+As a user, I want to be able to subtract one number from another.
+As a user, I want to be able to multiply two numbers together.
+As a user, I want to be able to divide one number by another.
+As a user, I want to be able to see the output of the mathematical operation.
+As a user, I want to be able to clear all operations and start from 0.
+
+*/
+
 /*-------------------------------- Constants --------------------------------*/
-
+const displayEl = document.querySelector('.display');
+const calculator = document.querySelector('#calculator');
+const buttons = document.querySelectorAll('.button');
 /*-------------------------------- Variables --------------------------------*/
-
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+  
+
 /*-------------------------------- Functions --------------------------------*/
+// function 
