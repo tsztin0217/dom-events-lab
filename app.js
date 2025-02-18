4e54fd0b83e2c705346b64fab4b076e1d5e65904
tsztin0217 <tsztin0217@gmail.com>
2025-02-17 23:02:03 -0500

    Started button function and calculate function

--- app.js
+++ app.js
@@ -20,7 +20,76 @@ const buttons = document.querySelectorAll('.button');
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      // This log is for testing purposes to verify we're getting the correct value
+      // console.log(event.target);
+      // output:
+      // <div class="button number">8</div>..etc
 
+      // console.log(event.target.classList)
+      const clickedEl = event.target;
+      if (clickedEl.classList.contains('number')) {
+        displayEl.innerText += clickedEl.innerText;
+      }
+    });
+  });
+
+function calculate (num1, num2, operator) {
+    switch(operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+
+    }
+
+}
+
+// calculator.addEventListener('click', (event) => {
+//     if (event.target.classList.contains('operator')) {
+//         if (event.target.innerText === 'C') {
+//             displayEl.innerText = '';
+            
+//         }
+//     // if (event.target.classList.contains('button')) {
+//     //     displayEl.innerText = displayEl.innerText += event.target.innerText;
+//     // }
+    
+//         calculate();
+// }
+// }
+// )
+
+
+// calculator.addEventListener('click', (event) => {
+//     // This log is for testing purposes to verify we're getting the correct value
+//     // You have to click a button to see this log
+//     // console.log(event.target.innerText);
+  
+//     // // Example
+//     if (event.target.classList.contains('button')) {
+//         displayEl.innerText = displayEl.innerText += event.target.innerText;
+//         // console.log(event.target.innerText)
+//         // console.log(displayEl.innerText)
+//       // Do something with a number
+//     }
+//     console.dir(event.target)
+
+//     // if (event.target.classList.contains('operator')) {
+//     //     if (event.target.innerText === '*') {
+
+//     //     }
+//     // }
+//     // // Example
+//     // if (event.target.innerText === '*') {
+//     //   // Do something with this operator
+//     }
+//   )
   
 
 /*-------------------------------- Functions --------------------------------*/
