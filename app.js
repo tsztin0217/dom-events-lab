bdc80a6491f94ef9c84ac12b19ae152d87543918
tsztin0217 <tsztin0217@gmail.com>
2025-02-18 07:45:21 -0500

    Updated calculate function to handle division by 0

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ function calculate(num1, operator, num2) {
         case '*':
             return num1 * num2;
         case '/':
+            if (num2 === 0) return 'Error'; // Return Error if divided by 0
             return num1 / num2;
         default:
             return num2;
